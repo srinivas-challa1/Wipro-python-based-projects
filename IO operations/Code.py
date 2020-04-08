217e6428bf251be451c4755f8d54aac51275594f
srinivas-challa1 <srinivas.srinivas.challa@gmail.com>
2020-04-08 17:57:22 +0530

    commit

--- IO operations/Code.py
+++ IO operations/Code.py
@@ -2,20 +2,24 @@ from datetime import datetime
 
 
 def SecretCode(file):
-    NumOfLines = str(len(file.readlines()))
+    lines = file.readlines()
+    NumOfLines = str(len(lines))
     getTime = ''
     if (len(NumOfLines) > 2):
         getTime = NumOfLines[:2]+":"+NumOfLines[2:]
     elif(len(NumOfLines) == 2):
         getTime = NumOfLines[:2]+":"+"00"
+    else:
+        getTime = "0"+NumOfLines+":"+"00"
+    words = [word for line in lines for word in line.split()]
+    Meeting_place = max(words, key=words.count)+" Street"
     d = datetime.strptime(getTime, "%H:%M")
     Meeting_time = d.strftime("%I:%M %p")
-    Meeting_place = max((file.txt).split(), key=(file.txt).count)
-    print("Meeting time:".format(Meeting_time))
-    print("Meeting place:".format(Meeting_place))
+    print("Meeting time:{}".format(Meeting_time))
+    print("Meeting place:{}".format(Meeting_place))
 
 
 if __name__ == "__main__":
     fname = input()
     file = open(fname, "r")
-    print(SecretCode(file))
+    SecretCode(file)
--- IO operations/test.py
+++
@@ -1,5 +0,0 @@
-fname = input()
-file = open(fname, "r")
-words = [word for line in file for word in line.split()]
-Meeting_place = max(words, key=words.count)+" Street"
-print(Meeting_place)
